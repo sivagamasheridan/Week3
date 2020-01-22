0658a43d2ca085f13ff86992b8e16deb941cd430
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2020-01-22 13:36:41 -0500

    example

---
+++ src/exampleweek3/ExampleWeek3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exampleweek3;
+
+/**
+ *
+ * @author srinivsi
+ */
+public class ExampleWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       System.out.println("learning pull operations");
+    }
+    
+}
